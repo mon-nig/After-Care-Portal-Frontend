e5ad14107ca9a0c7edfefb711a9edbcc4eca7227
Yasiru Sanjaya <ghyasirusanjaya@gmail.com>
2026-03-28 17:22:25 +0530

    Fix1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
-import { AuthProvider } from "@/contexts/auth-context";
+import { AuthProvider } from "../contexts/auth-context";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
